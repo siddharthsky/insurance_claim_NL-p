c252b4137bbf65c5bcc70f4024f8e4843b557ac6
Siddharth Kamble <siddharthsk7t@gmail.com>
2023-07-02 01:15:57 +0530

    - data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,13 @@
 import os
 import datetime as dt
-import numpy as np
 import pandas as pd
 from dataclasses import dataclass
 
 from sklearn.model_selection import train_test_split
 
 #Local Components
-from src.components.data_viz import VizStore
 from src.components.data_transformation import DataTransformation
+
 
 @dataclass
 class DataIngestionConfig():
@@ -20,13 +19,13 @@ class DataIngestionConfig():
     print(raw_data_path)
 
 class DataIngestion:
-    def __init__(self,raw_file):
+    def __init__(self):
         self.ingestion_config = DataIngestionConfig()
-        self.raw_file = raw_file
         self.sub = False
 
     def initialize_data_ingestion(self):
         try:
+            #Here goes the ingestion code for data
             df=pd.read_excel("notebooks\Dataset_Public_mini.xlsx")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -35,23 +34,24 @@ class DataIngestion:
 
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) # type: ignore
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True) # type: ignore
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True) 
 
-            return (
+            return ( #Gives test and train data 
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
                 )
         
         except Exception as e:
-            print("Error processing data")
+            print("Error processing data ingestion")
 
 
 
 if __name__ == "__main__":
-    #obj=DataIngestion()
-    #strain_data,test_data=obj.initialize_data_ingestion()
+    pass
+    obj=DataIngestion()
+    strain_data,test_data=obj.initialize_data_ingestion()
     #data_trasformer=DataTransformation()
     #train_arr,test_arr,_=data_trasformer.initialize_data_transformation(train_data,test_data)
     #modeltrainer=ModelTrainer()
