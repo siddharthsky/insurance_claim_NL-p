051a9d3e5b937897cf998bf0d6b6c3730de430ae
Siddharth Kamble <siddharthsk7t@gmail.com>
2023-07-02 02:11:01 +0530

    - main.py

--- highnet.py
+++
@@ -1,4 +0,0 @@
-from src.components.data_ingestion import DataIngestion
-
-obj=DataIngestion()
-strain_data,test_data=obj.initialize_data_ingestion()
--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-from flask import Flask, render_template, request
-import os 
-import shutil
-from src.pipeline.predict_pipeline import PredictPipeline
+from src.components.data_ingestion import DataIngestion
 
-#setting temp location
-tp = os.path.join(os.getcwd(),"static/temp")
-os.makedirs(os.path.dirname(tp),exist_ok=True)
-
-#Cleanup temporary files of previous sessions
-try:
-    shutil.rmtree("static/temp")
-except OSError as e:
-    print(f"Error: {e.filename} - {e.strerror}")
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/pred",methods=["POST"])
-def pred():
-    if request.method == "POST":
-        print(request)
-        f = request.files['file']
-        f.save(os.path.join(tp,f.filename)) # save file 
-        raw_filez = os.path.join(tp,f.filename) # full path of file
-        DataPred = PredictPipeline(raw_filez) # create DataIngestion object
-        return render_template("index.html",sub = DataPred.sub)
-
-if __name__ =="__main__":
-    app.run(debug=True)
+obj=DataIngestion()
+train_data,test_data=obj.initialize_data_ingestion()
+#data_trasformer=DataTransformation()
+#train_arr,test_arr,_=data_trasformer.initialize_data_transformation(train_data,test_data)
